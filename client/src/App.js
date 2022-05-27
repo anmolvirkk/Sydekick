819f54745f074a843193e02894820477c70659bf
Anmol Virk <theanmolvirk@gmail.com>
2022-05-27 15:46:51 +0530

    update

---
+++ client/src/App.js
@@ -0,0 +1,92 @@
+import styles from './_app.module.sass'
+import FeatherIcon from 'feather-icons-react'
+import Lottie from 'react-lottie-player'
+import * as data from './robot.json'
+
+const App = () => {
+
+  const Header = () => {
+    return (
+      <div className={styles.header}>
+        <img src='/logo.png' alt='' />
+        <div className={styles.dropdown}>
+          <p>Indeed</p>
+          <FeatherIcon icon='chevron-down' />
+        </div>
+      </div>
+    )
+  }
+
+  const Form = () => {
+    return (
+      <div className={styles.form}>
+        <div className={styles.auth}>
+          <input type='text' placeholder='email' id='email' />
+          <input type='text' placeholder='password' id='password' />
+        </div>
+        <div className={styles.search}>
+          <input type='text' placeholder='search query' id='what' />
+          <input type='text' placeholder='location' id='where' />
+        </div>
+        <div className={styles.buttons}>
+          <label>
+              <div className={styles.content}>
+                  <FeatherIcon icon='upload' />
+                  <p>Upload Resume</p>
+              </div>
+              <input type="file" />
+          </label>
+          <label>
+              <div className={styles.content}>
+                  <FeatherIcon icon='upload' />
+                  <p>Upload Cover Letter</p>
+              </div>
+              <input type="file" />
+          </label>
+        </div>
+      </div>
+    )
+  }
+
+  const start = () => {
+    fetch('http://localhost:5000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        what: document.getElementById('what').value,
+        where: document.getElementById('where').value
+      })
+    }).then((res)=>res.json()).then(data=>{
+      console.log(data)
+    })
+  }
+
+  return (
+    <div className={styles.app}>
+      <Header />
+      <div className={styles.hero}>
+        <Lottie
+          loop
+          play
+          animationData={data}
+          style={{
+            width: 180,
+            height: 180
+          }}
+        />
+        <div className={styles.content}>
+          <h1>Sydekick</h1>
+          <p>Automate Job Applications</p>
+        </div>
+      </div>
+      <Form />
+      <button onMouseDown={start}>Start</button>
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const playwright = require('playwright')
+const browserType = 'chromium'
+
+const site = 'https://au.indeed.com/'
+
+const main = async (req) => {
+  const browser = await playwright[browserType].launch({ headless: false })
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto(site)
+  await page.waitForLoadState('load')
+    
+  await page.waitForSelector('#text-input-what')
+  const whatInput = await page.$('#text-input-what')
+  await whatInput.type(req.body.what)
+
+  await page.waitForSelector('#text-input-where')
+  const whereInput = await page.$('#text-input-where')
+  await whereInput.type(req.body.where)
+  
+  await page.keyboard.press('Enter')
+  // await browser.close()
+}
+
+app.post('/', (req, res) => {
+  main(req).then(()=>{
+    res.send('test')
+  })
+})
+
+app.listen(5000, () => {
+  console.log(`Running on port 5000.`)
+})
